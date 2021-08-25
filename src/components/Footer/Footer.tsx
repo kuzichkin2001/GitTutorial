317b92fa29d3f5bba97565f12e33e7304709c694
kuzichkin2001 <kuzichkin.p@yandex.ru>
2021-08-25 21:08:01 +0400

    little fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,31 +1,30 @@
 import React, {useState} from "react";
 import './Footer.css';
-function Footer(){
-    return(
+
+function Footer() {
+    return (
         <footer>
             <div className="footer-content">
                 <p className="footer-copyright">&copy; 2021 BongaGuides.com</p>
-            <ul className="footer-content__list">
-                <li className="footer-content__list-item">
-                    <a className="footer-content__link" href="https://www.sgu.ru/">SSU</a>
-                </li>
-                <li className="footer-content__list-item">
-                    <a className="footer-content__link" href="rules">Terms of Use</a>
-                </li>
-                <li className="footer-content__list-item">
-                    <a className="footer-content__link" href="contacts">Contact Us</a>
-                </li>
-                <li className="footer-content__list-item">
-                    <a className="footer-content__link" href="subscribe">Subscribe to our updates </a>
-                </li>
-                <li className="footer-content__list-item">
-                    <a className="footer-content__link" href="#">Scroll to top</a>
-                </li>
-            </ul>
+                <ul className="footer-content__list">
+                    <li className="footer-content__list-item">
+                        <a className="footer-content__link" href="https://www.sgu.ru/">SSU</a>
+                    </li>
+                    <li className="footer-content__list-item">
+                        <a className="footer-content__link" href="rules">Terms of Use</a>
+                    </li>
+                    <li className="footer-content__list-item">
+                        <a className="footer-content__link" href="contacts">Contact Us</a>
+                    </li>
+                    <li className="footer-content__list-item">
+                        <a className="footer-content__link" href="subscribe">Subscribe to our updates </a>
+                    </li>
+                    <li className="footer-content__list-item">
+                        <a className="footer-content__link" href="#">Scroll to top</a>
+                    </li>
+                </ul>
             </div>
         </footer>
-
-    )
-
+    );
 }
 export default Footer;
