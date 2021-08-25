3f0e6db44dcd22889883cc0b82c169fa935e78fd
MasterOfLevan <lalka1999q@gmail.com>
2021-08-25 16:31:30 +0400

    Ded napilsya tabletok ot golovi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import BodyM from "./components/BodyM/BodyM";
+import Footer from "./components/Footer/Footer";
 function App() {
   const [title, setTitle] = useState<string>('');
 
@@ -13,6 +14,7 @@ function App() {
     <div className="container">
       <Header/>
       <BodyM/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import './Footer.css';
+import Header from "../Header/Header";
+function Footer(){
+    return(
+        <footer>
+            <div className="footer-content">
+                <p className="footer-copyright">&copy; 2021 BongaGuides.com</p>
+            <ul className="footer-content__list">
+                <li className="footer-content__list-item">
+                    <a className="footer-content__link" href="https://www.sgu.ru/">SSU</a>
+                </li>
+                <li className="footer-content__list-item">
+                    <a className="footer-content__link" href="rules">Terms of Use</a>
+                </li>
+                <li className="footer-content__list-item">
+                    <a className="footer-content__link" href="contacts">Contact Us</a>
+                </li>
+                <li className="footer-content__list-item">
+                    <a className="footer-content__link" href="subscribe">Subscribe to our updates </a>
+                </li>
+                <li className="footer-content__list-item">
+                    <a className="footer-content__link" href="#">Scroll to top</a>
+                </li>
+            </ul>
+            </div>
+        </footer>
+
+    )
+
+}
+export default Footer;
